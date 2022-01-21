07068567e16b5fc27b038384b82758fc4b9efb85
Go Compiler Team <pkg-golang-devel@lists.alioth.debian.org>
2022-01-21 18:44:56 +0000

    CVE-2021-41771
    Origin: https://github.com/golang/go/commit/d19c5bdb24e093a2d5097b7623284eb02726cede
    Reviewed-by: Sylvain Beucler <beuc@debian.org>
    Last-Update: 2022-01-21
    
    From d19c5bdb24e093a2d5097b7623284eb02726cede Mon Sep 17 00:00:00 2001
    From: Roland Shoemaker <roland@golang.org>
    Date: Thu, 14 Oct 2021 13:02:01 -0700
    Subject: [PATCH] [release-branch.go1.16] debug/macho: fail on invalid dynamic
     symbol table command
    MIME-Version: 1.0
    Content-Type: text/plain; charset=UTF-8
    Content-Transfer-Encoding: 8bit
    
    Fail out when loading a file that contains a dynamic symbol table
    command that indicates a larger number of symbols than exist in the
    loaded symbol table.
    
    Thanks to Burak Çarıkçı - Yunus Yıldırım (CT-Zer0 Crypttech) for
    reporting this issue.
    
    Updates #48990
    Fixes #48991
    Fixes CVE-2021-41771
    
    Change-Id: Ic3d6e6529241afcc959544b326b21b663262bad5
    Reviewed-on: https://go-review.googlesource.com/c/go/+/355990
    Reviewed-by: Julie Qiu <julie@golang.org>
    Reviewed-by: Katie Hockman <katie@golang.org>
    Reviewed-by: Emmanuel Odeke <emmanuel@orijtech.com>
    Run-TryBot: Roland Shoemaker <roland@golang.org>
    TryBot-Result: Go Bot <gobot@golang.org>
    Trust: Katie Hockman <katie@golang.org>
    (cherry picked from commit 61536ec03063b4951163bd09609c86d82631fa27)
    Reviewed-on: https://go-review.googlesource.com/c/go/+/359454
    Reviewed-by: Dmitri Shuralyov <dmitshur@golang.org>
    
    Gbp-Pq: Name CVE-2021-41771.patch

--- src/debug/macho/file_test.go
+++ src/debug/macho/file_test.go
@@ -208,3 +208,10 @@ func TestOpenFatFailure(t *testing.T) {
 		t.Errorf("OpenFat %s: got %v, want nil", filename, ff)
 	}
 }
+
+func TestOpenBadDysymCmd(t *testing.T) {
+	_, err := Open("testdata/gcc-amd64-darwin-exec-with-bad-dysym")
+	if err == nil {
+		t.Fatal("openObscured did not fail when opening a file with an invalid dynamic symbol table command")
+	}
+}
